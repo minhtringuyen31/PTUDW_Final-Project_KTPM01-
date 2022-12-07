42a4fd0e562866860e28de6e5664600ee5dfd13c
PHoaiLe <lehoaiphuong13042002@gmail.com>
2022-12-07 17:17:30 +0700

    change index of product

--- components/user/products/index.js
+++ components/user/products/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const productController = require('./productController');
 
-
 router.get('/', function(req, res) {
-    res.render('user/products/list',  { layout: 'layout.hbs' });
-  });
+  res.render('user/products/general', {layout: 'layout.hbs'});
+})
 
-  router.get('/:productID', productController.productDetail);
+router.get('/list', productController.allProductList);
+
+router.get('/detail', productController.productDetail);
 
 module.exports = router;
 
