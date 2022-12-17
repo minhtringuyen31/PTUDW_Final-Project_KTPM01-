cd1737fd4895f854c6dbdb3cc700f82a39556975
Nguyen Vo Minh Tri <minhtri.nguyenvo31@gmail.com>
2022-12-17 10:22:11 +0700

    update index app

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productRouter = require('./components/user/products');
-const accountRouter = require('./components/user/accounts');
-const accountAdminRouter = require('./components/admin/accounts');
+// const accountRouter = require('./components/user/accounts');
+// const accountAdminRouter = require('./components/admin/accounts');
 const dashboardRouter = require('./components/admin');
+const authRouter = require('./components/authentication');
 const customerRouter = require('./components/admin/customers');
 const manageProductRouter = require('./components/admin/products');
+const accountRouter = require('./components/user/accounts');
+
+
+
+const passport = require('./components/authentication/passport');
 
 const app = express();
 
@@ -19,35 +26,60 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(session({
+  secret: 'very secret keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.authenticate('session'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(function (req, res, next) {
+  console.log("res.user");
+  console.log(req.user);
+  res.locals.user = req.user;
+  next();
+});
+
+
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 // app.use('/students', studentRouter);
 app.use('/products', productRouter);
-app.use('/account', accountRouter);
-app.use('/account_admin',accountAdminRouter);
+app.use('/auth', authRouter);
+// app.use('/account_admin',accountAdminRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/customer', customerRouter);
-app.use('/manageProduct',manageProductRouter)
+app.use('/manageProduct', manageProductRouter)
+app.use('/account', accountRouter);
+
+//passport
+
+
+
+// app.use(passport.initialize());
+
 
 
 // catch 404 and forward to error handler
-app.use(function(req,
-                 res,
-                 next) {
+app.use(function (req,
+  res,
+  next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err,
-                 req,
-                 res,
-                 next) {
+app.use(function (err,
+  req,
+  res,
+  next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
