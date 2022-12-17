d92083c9762ea8294a1ac12015f3ab1933fdc80b
PHoaiLe <lehoaiphuong13042002@gmail.com>
2022-12-17 09:59:16 +0700

    change account index

--- components/user/accounts/index.js
+++ components/user/accounts/index.js
@@ -2,22 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 
-// router.get('/', function(req, res, next) {
-//     //throw new Error('Unknown error!');
-//     res.redirect('user/logIn')
-//   });
-  router.post('/', function(req, res, next) {
+router.get('/admin', function(req, res, next) {
     //throw new Error('Unknown error!');
-    res.render('indexFrontEnd');
+    res.redirect('/admin-login');
+    //res.render('admin/admin.hbs', { title: 'admin' });
+  });
+  router.post('/admin', function(req, res, next) {
+    //throw new Error('Unknown error!');
+    res.render('admin/admin', { title: 'admin' });
+  });
+  router.get('/login_admin', function(req, res, next) {
+    //throw new Error('Unknown error!');
+    res.render('admin/accounts/logIn', {layout: false});
   });
 
-  router.get('/login', function(req, res, next) {
+  router.get('/profile', function(req, res) {
     //throw new Error('Unknown error!');
-    res.render('user/accounts/logIn', { layout: false });
-  });
-  router.get('/signup', function(req, res, next) {
-    //throw new Error('Unknown error!');
-    res.render('user/accounts/signUp', { layout: false });
+    res.render('admin/accounts/adminProfile', {layout: "layoutAdmin"});
   });
 
-  module.exports = router;
+
+  module.exports=router;
