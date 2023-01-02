5d4e1aa3c8e0d44c436af5309fbb6303cb1dd38a
thien <79550249+thienhk15@users.noreply.github.com>
2023-01-02 23:12:41 +0700

    fix register

--- components/auth/authController.js
+++ components/auth/authController.js
@@ -22,18 +22,21 @@ exports.signup = async (req, res, next) => {
     //check format
     if(authService.checkSignUpFormat(req.body) === false)
     {
+        
         res.render('auth/signUp', {layout: false, error: {phone: "Phone number must be 10 characters", pass: "password must be more than 8 characters! "}});
         return;
     }
     //after checking format, check if the phone number has already existed
-    const check2 = await authService.isExistedAccount(req.body);
+    const check2 = await authService.isExistedAccount(req.body.userPhone);
     if(check2 == true)
     {
+        
         res.render('auth/signup', {layout: false, error: {phone: "The phone number was registered.", pass: null}});
         return;
     }
     else
     {
+        
         authService.register(req.body);
         res.redirect("auth/logIn");
     }
